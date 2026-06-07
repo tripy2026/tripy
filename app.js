8b5791635cbb2d34ce9172d281752bcadd3e0aaa
tripy2026 <tripy06062026@gmail.com>
2026-06-07 17:27:33 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,78 @@
+import { app } from "./firebase.js";
+
+function calculateFare(){
+
+let km =
+document.getElementById("km").value;
+
+let fare =
+20 + (km*8);
+
+document.getElementById("fare")
+.innerHTML =
+"Estimated Fare ₹" + fare;
+
+}
+
+window.calculateFare =
+calculateFare;
+
+function getLocation(){
+
+navigator.geolocation
+.getCurrentPosition(
+
+(position)=>{
+
+document.getElementById(
+"location"
+).innerHTML =
+
+position.coords.latitude +
+", " +
+position.coords.longitude;
+
+});
+
+}
+
+window.getLocation =
+getLocation;
+
+let map =
+L.map('map')
+.setView([25.4484,78.5685],13);
+
+L.tileLayer(
+'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+).addTo(map);
+
+function bookRide(){
+
+let pickup =
+document.getElementById(
+"pickup"
+).value;
+
+let drop =
+document.getElementById(
+"drop"
+).value;
+
+let msg =
+
+`Tripy Ride Booking
+
+Pickup: ${pickup}
+
+Drop: ${drop}`;
+
+window.open(
+"https://wa.me/919236764481?text="
++ encodeURIComponent(msg)
+);
+
+}
+
+window.bookRide =
+bookRide;

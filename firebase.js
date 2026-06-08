79892ddc29f6b4c6d402c8b98510d4d1f6337f68
tripy2026 <tripy06062026@gmail.com>
2026-06-08 11:05:52 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,21 @@
-import { app } from "./firebase.js";
 import { initializeApp }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 const firebaseConfig = {
 
-apiKey: "AIzaSyCM_wNzASy9qwcWGUfRo_inQzJtSG5TadA",
+  apiKey: "AIzaSyCM_wNzASy9qwcWGUfRo_inQzJtSG5TadA",
 
-authDomain: "tripy-4d596.firebaseapp.com",
+  authDomain: "tripy-4d596.firebaseapp.com",
 
-projectId: "tripy-4d596",
+  projectId: "tripy-4d596",
 
-storageBucket: "tripy-4d596.firebasestorage.app",
+  storageBucket: "tripy-4d596.firebasestorage.app",
 
-messagingSenderId: "38146514812",
+  messagingSenderId: "38146514812",
 
-appId: "1:38146514812:web:90582eed0bea0f69e50ef9",
+  appId: "1:38146514812:web:90582eed0bea0f69e50ef9",
 
-measurementId: "G-7L4YKH4TCZ"
+  measurementId: "G-7L4YKH4TCZ"
 
 };
 
